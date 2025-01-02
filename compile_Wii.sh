daba9df690dd2e5d7bf46cfb6c6b3e2ade83360d
Wiibuntu <146908244+Wiibuntu@users.noreply.github.com>
2025-01-01 20:20:07 -0500

    update compile

--- compile_Wii.sh
+++ compile_Wii.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+mkdir deb/usr/local/bin
 /bin/powerpc-linux-gnu-gcc -o deb/usr/local/bin/wiibuntu wiibuntu.c -lX11 -lpng
 
--- compile_x86_test.sh
+++ compile_x86_test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/bin/x86_64-pc-linux-gnu-gcc -o wiibuntu wiibuntu.c -lX11 -lpng
+mkdir deb/usr/local/bin
+/bin/x86_64-pc-linux-gnu-gcc -o deb/usr/local/bin/wiibuntu wiibuntu.c -lX11 -lpng
 
